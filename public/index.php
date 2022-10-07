a5d4ff340523d7294042f26ad380462c44427999
38672505883 <allan.shibata@gmail.com.br>
2022-10-07 09:08:11 -0300

    Atualizando

--- public/index.php
+++ public/index.php
@@ -5,4 +5,14 @@ use Src\models\User;
 require_once "../vendor/autoload.php";
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/views/login.php');
+//require_once(dirname(__FILE__, 2) . '/src/views/login.php');
+
+
+$user = User::get();
+$teste = $user > 5 ? 'Tem registro' : 'Não tem registro';
+foreach ($user AS $item => $value) {
+    echo "<pre>";
+    print_r($value);
+    echo "</pre>";
+}
+
