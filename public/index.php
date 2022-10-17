1e71cd60ce31da643c169126e8681710e708c32b
38672505883 <allan.shibata@gmail.com.br>
2022-10-17 10:34:35 -0300

    Version 1.0.0

--- public/index.php
+++ public/index.php
@@ -9,16 +9,6 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 /**
  * Layout
  */
-require_once(VIEW_PATH . '/login.php');
+//require_once(CONTROLLER_PATH . '/login.php');
 
-
-$user = new Login([
-    'email' => 'admin@uniponto.com.br',
-    'password' => 'a'
-]);
-
-try {
-    $user->checkLogin();
-} catch (\Exception $exception) {
-    echo "Problema no login";
-}
+loadView('login', ['texto' => 'abc123']);
