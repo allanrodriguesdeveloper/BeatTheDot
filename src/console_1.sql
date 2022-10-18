173736dfbb979068f75238672ebf7caf86b7dbab
38672505883 <allan.shibata@gmail.com.br>
2022-10-18 10:48:24 -0300

    Initial Structure

--- src/console_1.sql
+++
@@ -1,9 +0,0 @@
-SELECT *
-FROM users;
-
-UPDATE users
-SET email = 'admin@admin.com.br'
-WHERE id = 1
-
-INSERT INTO portaria_controle_historico
-VALUES (uuid_generate_v4(), $1, $2, $3, now(), null, $4)
