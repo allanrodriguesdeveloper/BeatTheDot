b33c1286f6ecce8cc46c5c4a1773826f40d29976
38672505883 <allan.shibata@gmail.com.br>
2022-10-18 14:56:46 -0300

    :memo: Class User #2

--- src/config/Database.php
+++ src/config/Database.php
@@ -6,33 +6,33 @@ class Database
      * Database connection - data coming from env.ini
      * Conexão com o banco de dados - dados vindo do env.ini
      */
-    public static function getConnection()
+    private static function getConnection(): mysqli
     {
         $envPath = realpath(dirname(__FILE__) . '/../env.ini');
         $env = parse_ini_file($envPath);
-        $conn = new mysqli(
+        $connection = new mysqli(
             $env['host'],
             $env['username'],
             $env['password'],
             $env['database']
         );
 
-        if ($conn->connect_error) {
-            die("Error:" . $conn->connect_error);
+        if ($connection->connect_error) {
+            return $connection->connect_error;
         }
 
-        return $conn;
+        return $connection;
     }
 
     /**
      * Retrieve the result from the database
      * Recupera o resultado do banco de dados
      */
-    public static function getResultFromQuery($sql)
+    public static function getResultFromQuery(string $querySql)
     {
-        $conn = self::getConnection();
-        $result = $conn->query($sql);
-        $conn->close();
+        $connection = self::getConnection();
+        $result = $connection->query($querySql);
+        $connection->close();
         return $result;
     }
 }
